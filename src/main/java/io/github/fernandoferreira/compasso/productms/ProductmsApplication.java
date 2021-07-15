28d9de4bd08ae2deb2b009c41348e6afe33f6834
Fernando Ferreira <fernando.ferreira.tbe@gmail.com>
2021-07-15 16:03:24 -0300

    Initial commit

---
+++ src/main/java/io/github/fernandoferreira/compasso/productms/ProductmsApplication.java
@@ -0,0 +1,13 @@
+package io.github.fernandoferreira.compasso.productms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/fernandoferreira/compasso/productms/ProductmsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.fernandoferreira.compasso.productms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
