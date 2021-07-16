2776ee62b4668116832552d9730de1ecb8d9d832
Fernando Ferreira <fernando.ferreira.tbe@gmail.com>
2021-07-16 17:24:54 -0300

    Implement initial get by id operation

--- src/main/java/io/github/fernandoferreira/compasso/productms/controller/ProductController.java
+++ src/main/java/io/github/fernandoferreira/compasso/productms/controller/ProductController.java
@@ -5,10 +5,12 @@ import io.github.fernandoferreira.compasso.productms.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/products")
@@ -24,5 +26,12 @@ public class ProductController {
     @GetMapping
     public ResponseEntity<List<Product>> getProducts() {
         return ResponseEntity.ok(this.productService.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> findById(@PathVariable Long id) {
+        Optional<Product> optional = this.productService.findById(id);
+
+        return optional.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 }
--- src/main/java/io/github/fernandoferreira/compasso/productms/service/ProductService.java
+++ src/main/java/io/github/fernandoferreira/compasso/productms/service/ProductService.java
@@ -3,9 +3,12 @@ package io.github.fernandoferreira.compasso.productms.service;
 import io.github.fernandoferreira.compasso.productms.model.Product;
 import io.github.fernandoferreira.compasso.productms.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ProductService {
@@ -19,5 +22,9 @@ public class ProductService {
 
     public List<Product> findAll() {
         return this.productRepository.findAll();
+    }
+
+    public Optional<Product> findById(Long id) {
+        return this.productRepository.findById(id);
     }
 }
