99286b34fe913871e8b2f60726b11ea38df44b28
Fernando Ferreira <fernando.ferreira.tbe@gmail.com>
2021-07-16 15:47:07 -0300

    Configure product entity

--- src/main/java/io/github/fernandoferreira/compasso/productms/model/Product.java
+++ src/main/java/io/github/fernandoferreira/compasso/productms/model/Product.java
@@ -2,17 +2,32 @@ package io.github.fernandoferreira.compasso.productms.model;
 
 import lombok.*;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @ToString
 @EqualsAndHashCode
+@Entity
 public class Product {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotNull @NotEmpty @Size(min = 3, max = 100)
     private String name;
+    @NotNull @NotEmpty @Size(min = 3, max = 100)
     private String description;
+    @NotNull @NotEmpty @Min(value = 0)
     private Double price;
 
 }
