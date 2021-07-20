c23173a66aa197f22a6af8545d0bffb9f8739fa9
Fernando Ferreira <fernando.ferreira.tbe@gmail.com>
2021-07-19 22:44:00 -0300

    Implement api documentation by swagger

--- src/main/java/io/github/fernandoferreira/compasso/productms/ProductmsApplication.java
+++ src/main/java/io/github/fernandoferreira/compasso/productms/ProductmsApplication.java
@@ -2,8 +2,10 @@ package io.github.fernandoferreira.compasso.productms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class ProductmsApplication {
 
 	public static void main(String[] args) {
