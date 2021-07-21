7bcde7ff1e21129ef5742c2230c9079f7dc8c416
Fernando Ferreira <fernando.ferreira.tbe@gmail.com>
2021-07-20 23:16:42 -0300

    Implement repository delete method test

--- src/test/java/io/github/fernandoferreira/compasso/productms/repository/ProductRepositoryTest.java
+++ src/test/java/io/github/fernandoferreira/compasso/productms/repository/ProductRepositoryTest.java
@@ -11,6 +11,8 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.ActiveProfiles;
 
+import java.util.NoSuchElementException;
+
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @ActiveProfiles("homolog")
@@ -21,6 +23,8 @@ public class ProductRepositoryTest {
 
     @Autowired
     private TestEntityManager testEntityManager;
+
+
 
     @Test
     public void givenValidProduct_ShouldCreateResource_ReturnNewProduct() {
@@ -33,4 +37,14 @@ public class ProductRepositoryTest {
         Assertions.assertEquals("Casual", product.getDescription());
         Assertions.assertEquals(99.9, product.getPrice());
     }
+
+    @Test
+    public void givenValidProduct_ShouldDeleteSuccessfully() {
+        Product product = Product.builder().name("Nike").description("Casual").price(99.9).build();
+        this.productRepository.save(product);
+        this.productRepository.delete(product);
+
+        Assertions.assertThrows(NoSuchElementException.class, () -> this.productRepository.findById(product.getId()).get());
+    }
+
 }
