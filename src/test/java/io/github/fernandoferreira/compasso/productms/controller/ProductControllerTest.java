9a7f2a53e13218f67704cd35188e6a389f4461ad
Fernando Ferreira <fernando.ferreira.tbe@gmail.com>
2021-07-21 00:30:20 -0300

    Implement get by id for product controller request

---
+++ src/test/java/io/github/fernandoferreira/compasso/productms/controller/ProductControllerTest.java
@@ -0,0 +1,54 @@
+package io.github.fernandoferreira.compasso.productms.controller;
+
+import io.github.fernandoferreira.compasso.productms.model.Product;
+import io.github.fernandoferreira.compasso.productms.service.ProductService;
+
+import io.restassured.http.ContentType;
+import io.restassured.module.mockmvc.RestAssuredMockMvc;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.Optional;
+
+import static org.mockito.Mockito.when;
+
+@ActiveProfiles(value = "homolog")
+@WebMvcTest
+public class ProductControllerTest {
+
+    @Autowired
+    private ProductController productController;
+
+    @MockBean
+    private ProductService productService;
+
+    @BeforeEach
+    public void setUp() {
+        RestAssuredMockMvc.standaloneSetup(this.productController);
+    }
+
+    @Test
+    public void givenValidId_ShouldFindItInDatabase_ReturnProductSuccessfully() {
+        Optional<Product> product = Optional.of(Product.builder()
+                                                            .id(1L)
+                                                            .name("Nike")
+                                                            .description("Casual")
+                                                            .price(34.90)
+                                                            .build());
+
+        when(this.productService.findById(1L))
+                .thenReturn(product);
+
+        RestAssuredMockMvc.given()
+                .accept(ContentType.JSON)
+                .when()
+                .get("/products/{id}", 1L)
+                .then()
+                .statusCode(HttpStatus.OK.value());
+    }
+}
