a022951576783c89d03716ded88cf920be420457
Fernando Ferreira <fernando.ferreira.tbe@gmail.com>
2021-07-20 23:04:59 -0300

    Implement repository save method test

---
+++ src/test/java/io/github/fernandoferreira/compasso/productms/repository/ProductRepositoryTest.java
@@ -0,0 +1,36 @@
+package io.github.fernandoferreira.compasso.productms.repository;
+
+import io.github.fernandoferreira.compasso.productms.config.exception.ProductNotFoundException;
+import io.github.fernandoferreira.compasso.productms.model.Product;
+import io.github.fernandoferreira.compasso.productms.service.ProductService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.ActiveProfiles;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("homolog")
+public class ProductRepositoryTest {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Autowired
+    private TestEntityManager testEntityManager;
+
+    @Test
+    public void givenValidProduct_ShouldCreateResource_ReturnNewProduct() {
+        Product product = Product.builder().name("Nike").description("Casual").price(99.9).build();
+        this.productRepository.save(product);
+
+        Assertions.assertNotNull(product.getId());
+        Assertions.assertTrue(product.getId() > 0);
+        Assertions.assertEquals("Nike", product.getName());
+        Assertions.assertEquals("Casual", product.getDescription());
+        Assertions.assertEquals(99.9, product.getPrice());
+    }
+}
