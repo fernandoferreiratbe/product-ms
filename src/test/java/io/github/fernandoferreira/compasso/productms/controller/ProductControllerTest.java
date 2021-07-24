50b312c1e0ea13e090a43029f6fbc98360b076e1
Fernando Ferreira <fernando.ferreira.tbe@gmail.com>
2021-07-23 22:02:22 -0300

    Implement product post unit test

--- src/test/java/io/github/fernandoferreira/compasso/productms/controller/ProductControllerTest.java
+++ src/test/java/io/github/fernandoferreira/compasso/productms/controller/ProductControllerTest.java
@@ -4,6 +4,9 @@ import io.github.fernandoferreira.compasso.productms.model.Product;
 import io.github.fernandoferreira.compasso.productms.service.ProductService;
 import io.restassured.http.ContentType;
 import io.restassured.module.mockmvc.RestAssuredMockMvc;
+import io.restassured.module.mockmvc.response.MockMvcResponse;
+import org.json.JSONObject;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -60,5 +63,32 @@ public class ProductControllerTest {
                 .get("/products/{id}", 5L)
                 .then()
                 .statusCode(HttpStatus.NOT_FOUND.value());
+    }
+
+    @Test
+    public void givenCorrectProductRequest_ShouldCreateNewResource_ReturnNewProduct() throws Exception {
+        Product product = Product.builder().name("SHIRT").description("MOUNTAIN SHIRT").price(90.0).build();
+        Product savedProduct = Product.builder().id(1L).name("SHIRT").description("MOUNTAIN SHIRT").price(90.0).build();
+
+        when(this.productService.save(product)).thenReturn(savedProduct);
+        JSONObject requestBody = new JSONObject();
+        requestBody.put("name", "SHIRT");
+        requestBody.put("description", "MOUNTAIN SHIRT");
+        requestBody.put("price", 90.0);
+
+        MockMvcResponse response = RestAssuredMockMvc.given()
+                .header("Content-Type", "application/json")
+                .and()
+                .body(requestBody.toString())
+                .when()
+                .post("/products")
+                .then()
+                .extract().response();
+
+        Assertions.assertEquals(201, response.statusCode());
+        Assertions.assertEquals("SHIRT", response.jsonPath().getString("name"));
+        Assertions.assertEquals("MOUNTAIN SHIRT", response.jsonPath().getString("description"));
+        Assertions.assertEquals("90.0", response.jsonPath().getString("price"));
+        Assertions.assertEquals("1", response.jsonPath().getString("id"));
     }
 }
