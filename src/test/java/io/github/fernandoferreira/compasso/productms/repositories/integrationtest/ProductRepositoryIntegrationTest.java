2a6291c674b83b061acce60a4168903f1f9c0c74
Fernando Ferreira <fernando.ferreira.tbe@gmail.com>
2022-04-24 23:00:20 -0300

    Fix product repository integreation test
    - Remove unused annotation that allowed database H2 init data to be loaded twice.

--- src/test/java/io/github/fernandoferreira/compasso/productms/repositories/integrationtest/ProductRepositoryIntegrationTest.java
+++ src/test/java/io/github/fernandoferreira/compasso/productms/repositories/integrationtest/ProductRepositoryIntegrationTest.java
@@ -1,75 +1,71 @@
-//package io.github.fernandoferreira.compasso.productms.repositories.it;
-//
-//import io.github.fernandoferreira.compasso.productms.models.Product;
-//import io.github.fernandoferreira.compasso.productms.repositories.ProductRepository;
-//import org.junit.jupiter.api.Assertions;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-//import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-//import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
-//import org.springframework.test.annotation.DirtiesContext;
-//import org.springframework.test.context.ActiveProfiles;
-//
-//import java.util.NoSuchElementException;
-//
-//@DataJpaTest
-//@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-//@ActiveProfiles("integration-test")
-////@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
-//class ProductRepositoryIntegrationTest {
-//
-//    @Autowired
-//    private ProductRepository productRepository;
-//
-//    @Autowired
-//    private TestEntityManager testEntityManager;
-//
-//    @Test
-//    void givenValidProduct_ShouldCreateResource_ReturnNewProduct() {
-//        Product product = Product.builder().name("Nike").description("Casual").price(99.9).build();
-//        this.productRepository.save(product);
-//
-//        Assertions.assertNotNull(product.getId());
-//        Assertions.assertTrue(product.getId() > 0);
-//        Assertions.assertEquals("Nike", product.getName());
-//        Assertions.assertEquals("Casual", product.getDescription());
-//        Assertions.assertEquals(99.9, product.getPrice());
-//    }
-//
-//    @Test
-//    void givenValidProduct_ShouldDeleteSuccessfully() {
-//        Product product = Product.builder().name("Nike").description("Casual").price(99.9).build();
-//        this.productRepository.save(product);
-//        this.productRepository.delete(product);
-//
-//        Assertions.assertThrows(NoSuchElementException.class, () -> this.productRepository.findById(product.getId()).get());
-//    }
-//
-//    @Test
-//    void givenProduct_UpdateItsValues_ShouldPersistCorrectly() {
-//        Product product = Product.builder().name("Nike").description("Casual").price(99.9).build();
-//        this.productRepository.save(product);
-//        Long id = product.getId();
-//        product.setName("Tennis Nike");
-//        product.setDescription("Not Casual");
-//        product.setPrice(349.60);
-//        this.productRepository.save(product);
-//        Product productUpdated = this.productRepository.findById(id).get();
-//
-//        Assertions.assertNotNull(productUpdated.getId());
-//        Assertions.assertEquals(id, productUpdated.getId());
-//        Assertions.assertEquals("Tennis Nike", productUpdated.getName());
-//        Assertions.assertEquals("Not Casual", productUpdated.getDescription());
-//        Assertions.assertEquals(349.60, productUpdated.getPrice());
-//    }
-//
-//    @Test
-//    void givenValidId_ShouldFindProductInDatabase_ReturnValidProduct() {
-//        Product product = Product.builder().name("Nike").description("Casual").price(99.9).build();
-//        this.productRepository.save(product);
-//
-//        Assertions.assertDoesNotThrow(() -> this.productRepository.findById(product.getId()).get());
-//
-//    }
-//}
+package io.github.fernandoferreira.compasso.productms.repositories.integrationtest;
+
+import io.github.fernandoferreira.compasso.productms.models.Product;
+import io.github.fernandoferreira.compasso.productms.repositories.ProductRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.NoSuchElementException;
+
+@DataJpaTest
+@ActiveProfiles("integration-test")
+class ProductRepositoryIntegrationTest {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Autowired
+    private TestEntityManager testEntityManager;
+
+    @Test
+    void givenValidProduct_ShouldCreateResource_ReturnNewProduct() {
+        Product product = Product.builder().name("Nike").description("Casual").price(99.9).build();
+        this.productRepository.save(product);
+
+        Assertions.assertNotNull(product.getId());
+        Assertions.assertTrue(product.getId() > 0);
+        Assertions.assertEquals("Nike", product.getName());
+        Assertions.assertEquals("Casual", product.getDescription());
+        Assertions.assertEquals(99.9, product.getPrice());
+    }
+
+    @Test
+    void givenValidProduct_ShouldDeleteSuccessfully() {
+        Product product = Product.builder().name("Nike").description("Casual").price(99.9).build();
+        this.productRepository.save(product);
+        this.productRepository.delete(product);
+
+        Assertions.assertThrows(NoSuchElementException.class, () -> this.productRepository.findById(product.getId()).get());
+    }
+
+    @Test
+    void givenProduct_UpdateItsValues_ShouldPersistCorrectly() {
+        Product product = Product.builder().name("Nike").description("Casual").price(99.9).build();
+        this.productRepository.save(product);
+        Long id = product.getId();
+        product.setName("Tennis Nike");
+        product.setDescription("Not Casual");
+        product.setPrice(349.60);
+        this.productRepository.save(product);
+        Product productUpdated = this.productRepository.findById(id).get();
+
+        Assertions.assertNotNull(productUpdated.getId());
+        Assertions.assertEquals(id, productUpdated.getId());
+        Assertions.assertEquals("Tennis Nike", productUpdated.getName());
+        Assertions.assertEquals("Not Casual", productUpdated.getDescription());
+        Assertions.assertEquals(349.60, productUpdated.getPrice());
+    }
+
+    @Test
+    void givenValidId_ShouldFindProductInDatabase_ReturnValidProduct() {
+        Product product = Product.builder().name("Nike").description("Casual").price(99.9).build();
+        this.productRepository.save(product);
+
+        Assertions.assertDoesNotThrow(() -> this.productRepository.findById(product.getId()).get());
+
+    }
+}
