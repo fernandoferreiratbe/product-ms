674eadf9d66de006e4c1727854d539005beb9cf4
Fernando Ferreira <fernando.ferreira.tbe@gmail.com>
2022-10-15 21:59:05 -0300

    Implement findById unit test for product service
    - Implement a unit test for findById method, mocking repository and
    refactor product mocked object.

--- src/test/java/io/github/fernandoferreira/compasso/productms/services/unittest/ProductServiceTest.java
+++ src/test/java/io/github/fernandoferreira/compasso/productms/services/unittest/ProductServiceTest.java
@@ -14,6 +14,7 @@ import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 
 @DisplayName("Product Service Test")
@@ -28,7 +29,7 @@ class ProductServiceTest {
 
     @Test
     @DisplayName("Invoke find all service method should return products.")
-    void invokeFindAll_ShouldFindOneProduct_ObjectStateReceivedAsExpected() {
+    void findAll_shouldFindOneProduct_ObjectStateReceivedAsExpected() {
         Mockito.when(this.productRepository.findAll()).thenReturn(this.createProducts());
 
         Set<Product> products = this.productService.findAll();
@@ -41,14 +42,28 @@ class ProductServiceTest {
 
     }
 
+    @Test
+    @DisplayName("Invoke findById passing valid product id should return a valid product")
+    void findById_givenValidProductId_shouldFindProduct_returnFoundProduct() {
+        Mockito.when(this.productRepository.findById(1L)).thenReturn(Optional.of(this.createProduct()));
+
+        Optional<Product> product = this.productService.findById(1L);
+
+        Assertions.assertThat(product.isPresent()).isTrue();
+        Assertions.assertThat(product.get().getId()).isEqualTo(1L);
+        Assertions.assertThat(product.get().getName()).isEqualTo("DUMMY_PRODUCT_NAME");
+    }
+
     private List<Product> createProducts() {
-        Product product = Product.builder()
+        return List.of(this.createProduct());
+    }
+
+    private Product createProduct() {
+        return Product.builder()
                 .id(1L)
                 .name("DUMMY_PRODUCT_NAME")
                 .description("DUMMY_PRODUCT_DESCRIPTION")
                 .price(230.0)
                 .build();
-
-        return List.of(product);
     }
 }
