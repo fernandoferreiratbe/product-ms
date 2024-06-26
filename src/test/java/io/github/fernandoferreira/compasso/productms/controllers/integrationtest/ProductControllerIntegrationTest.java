943ae32df5944cc363c59692804a3a97b8fafcfc
Fernando Ferreira <fernando.ferreira.tbe@gmail.com>
2024-06-26 20:00:44 -0300

    test: Fix product controller integration test

--- src/test/java/io/github/fernandoferreira/compasso/productms/controllers/integrationtest/ProductControllerIntegrationTest.java
+++ src/test/java/io/github/fernandoferreira/compasso/productms/controllers/integrationtest/ProductControllerIntegrationTest.java
@@ -1,23 +1,34 @@
 package io.github.fernandoferreira.compasso.productms.controllers.integrationtest;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.github.fernandoferreira.compasso.productms.controllers.dto.ProductRequest;
+import io.github.fernandoferreira.compasso.productms.models.Product;
 import io.restassured.RestAssured;
 import io.restassured.config.JsonConfig;
+import io.restassured.http.ContentType;
 import io.restassured.parsing.Parser;
 import io.restassured.path.json.config.JsonPathConfig;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.test.context.ActiveProfiles;
 
-import javax.annotation.PostConstruct;
+import jakarta.annotation.PostConstruct;
 
+import java.net.URI;
+
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@ActiveProfiles("integration-test")
+@ActiveProfiles("test")
+@Tag("integration")
 class ProductControllerIntegrationTest {
+
+    private final String PATH = "/products";
 
     @LocalServerPort
     private int port;
@@ -36,10 +47,10 @@ class ProductControllerIntegrationTest {
     void findAll_GivenValidRequest_ShouldReturnAllProductsAlreadyRegistered() {
         RestAssured
                 .when()
-                .get("/products")
+                    .get(PATH)
                 .then()
-                .assertThat()
-                .body("products.size()", is(3));
+                    .assertThat()
+                    .body("products.size()", is(3));
     }
 
     @Test
@@ -47,14 +58,37 @@ class ProductControllerIntegrationTest {
     void findById_GivenValidProductId_ShouldFindAndReturnValidProduct() {
         RestAssured
                 .when()
-                .get("/products/1")
+                    .get(PATH.concat("/1"))
                 .then()
-                .assertThat()
-                .statusCode(HttpStatus.OK.value())
-                .body("id", equalTo(1))
-                .body("name", equalTo("TENIS NIKE"))
-                .body("description", equalTo("PARA CORRIDAS LONGAS"))
-                .body("price", equalTo(350.0));
+                    .assertThat()
+                    .statusCode(HttpStatus.OK.value())
+                    .body("id", equalTo(1))
+                    .body("name", equalTo("TENIS NIKE"))
+                    .body("description", equalTo("PARA CORRIDAS LONGAS"))
+                    .body("price", equalTo(350.0));
     }
 
+    @Test
+    @DisplayName("Given a correct product request should create a new resource and return it")
+    void givenCorrectProductRequest_ShouldCreateNewResource_ReturnNewProduct() throws Exception {
+        ProductRequest productRequest = new ProductRequest(
+                "New Balance", "comfort for your entire day", 239.9
+        );
+
+        String requestBody = new ObjectMapper().writeValueAsString(productRequest);
+
+
+        Product productCreated = RestAssured
+                .given()
+                    .header("Content-Type", ContentType.JSON)
+                    .and()
+                    .body(requestBody)
+                .when()
+                    .post(PATH)
+                .then()
+                    .statusCode(HttpStatus.CREATED.value())
+                    .extract().as(Product.class);
+
+        assertThat(productCreated.getId(), notNullValue());
+    }
 }
